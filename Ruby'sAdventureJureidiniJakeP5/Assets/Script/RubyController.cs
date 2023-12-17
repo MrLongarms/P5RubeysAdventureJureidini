988062075aff287b985d5000d21b8eff430c225a
MrLongarms <145394433+MrLongarms@users.noreply.github.com>
2023-12-16 21:45:22 -0800

    Done

--- Ruby'sAdventureJureidiniJakeP5/Assets/Script/RubyController.cs
+++ Ruby'sAdventureJureidiniJakeP5/Assets/Script/RubyController.cs
@@ -61,6 +61,14 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
+
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit2d = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            {
+                Debug.Log("Raycast has hit the object " + hit2d.collider.gameObject);
+            }
+        }
     }
 
     void FixedUpdate()
@@ -86,7 +94,7 @@ public class RubyController : MonoBehaviour
             invincibleTimer = timeInvincible;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth/(float)maxHealth);
     }
 
 
